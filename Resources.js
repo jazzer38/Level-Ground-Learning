52ee5e5c0fed6bac0dd4476ce8c2dbb8388b8089
Jasmine Parran <jparran@unioncatholic.org>
2025-08-15 15:45:55 -0400

    Added resources page with html, css, and javaScript. Added all the logos and updated the fact slider

--- Resources.js
+++ Resources.js
@@ -1,1 +1,16 @@
-//Resources JavaScript
+document.addEventListener("DOMContentLoaded", function () {
+    //All details in FAQ
+    const faqItems = document.querySelector(".resources details");
+
+    faqItems.forEach(function(item) {
+        item.addEventListener("click", function() {
+            faqItems.forEach(function(otherItem) {
+                if (otherItem !== item) {
+                    otherItem.removeAttribute("open");
+                }
+            });
+
+    console.log("FAQ clicked:", item.querySelector("summary").tectContent);
+});
+    });
+});
